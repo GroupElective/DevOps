461780461ef68b27ce6b92bb30d58282071d3398
Jhon Carl Balanza <jhoncarlbalanza@gmail.com>
2019-10-28 12:55:07 +0800

    3rd commit

--- devOpsScript.sh
+++ devOpsScript.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-git add .
+read all
+git add $all
 read commit
 git commit -m "$commit"
 git status
 git remote add origin "https://github.com/GroupElective/DevOps.git"
 git push origin master
-jhoncarlgo
-FHFQT0Ly
-
